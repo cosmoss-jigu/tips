9843726cbd340b40fa4dd1d894a572cdc61b402a
madhavakrishnan <madhavakrishnan@vt.edu>
2021-07-15 15:25:27 -0400

    cleanup

--- tips/benchmark/art_wrapper.h
+++
@@ -1,8 +0,0 @@
-#include "ART.h"
-
-void *init_art_tree();
-char *tips_art_read(void *p_head, uint64_t target_key);
-int tips_art_insert(void *p_head, uint64_t target_key,char *value);
-int tips_art_remove(void *p_head, uint64_t target_key);
-int tips_art_scan(void *p_root, uint64_t start_key, int range, uint64_t *buff);
-
--- tips/tools/ordo/config.h
+++
@@ -1,3 +0,0 @@
-#define CACHE_BYTES 64
-#define MAX_CPU	    256
-#define ENABLE_PMC  0
